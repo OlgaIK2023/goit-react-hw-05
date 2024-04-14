10f8677eb4d62543f5b0d2af9c314b114855ad57
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-04-14 15:37:40 +0300

    upd

--- src/components/ScrollUp/ScrollUp.jsx
+++
@@ -1,9 +0,0 @@
-import { CiSaveUp2 } from "react-icons/ci";
-import css from "./ScrollUp.module.css"
-const ScrollUp = ({onScrollBtn}) => {
-  return (
-    <div className={css.scroll_up} onClick={onScrollBtn}><CiSaveUp2 size='50'/></div>
-  )
-}
-
-export default ScrollUp
